57f9c33594ff821ca463091ca124aa96e53b0285
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-09-09 23:38:27 +0200

    [UPD] Added link to style.css and conditional class to todo list items

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
     <!-- CSS -->
     <!-- bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- style -->
+     <link rel="stylesheet" href="./css/style.css">
     <!-- JS -->
      <!-- axios -->
      <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
@@ -25,7 +27,7 @@
             <div class="row">
                 <div class="col-12">
                     <ol>
-                        <li v-for="item, index in todoData" key="index">
+                        <li v-for="item, index in todoData" key="index" :class="item.done ? 'done' : ''">
                             {{ item.name }}
                         </li>
                     </ol>
